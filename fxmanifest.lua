6ad907d52d09bb1d3549c16ba5f308058a1b90d5
David Čudek <79476279+boofiboi@users.noreply.github.com>
2024-06-17 13:43:34 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'boofi#4375'
+author 'boofiboi'
 description 'Higly configurable discord rich presence script with animations etc!'
 version '1.1'
 
