2e606b04c56aba6d05f0dd3ff0d3ee34bfac456d
boofiboi <dajosek2seznam.cz@gmail.com>
2024-07-25 15:42:28 +0200

    Add error handling for update checker.

--- server/server.lua
+++ server/server.lua
@@ -5,6 +5,10 @@
 
 PerformHttpRequest('https://raw.githubusercontent.com/boofiboi/FiveM-Discord-Presence/main/VERSION', function(Error, OnlineVersion, Header)
     OfflineVersion = LoadResourceFile('FiveM-Discord-Presence', 'VERSION')
+    if Error then
+        error("\x1b[31m[ERROR]\x1b[97m There was an error, it is: " .. Error .. "\x1b[0m")
+        break
+    end
     if OnlineVersion <= OfflineVersion then -- Else if downloaded version file is the same then we dont need to update. 
     print('^3 [DEBUG]: \x1b[97m You are running the latest version of this script!. \x1b[0m')
     end
